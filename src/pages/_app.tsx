8554c662c7c8383bd0e8ad56a84399d07362536e
gefeiyanga <variousdid@gmail.com>
2023-04-04 17:36:32 +0800

    feat: 窗口大小自适应

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,13 @@ import { ChakraProvider, Container } from "@chakra-ui/react";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
+  const handleResize = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+  };
+  useEffect(() => {
+    window.addEventListener("resize", handleResize, false);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
   return (
     <ChakraProvider>
