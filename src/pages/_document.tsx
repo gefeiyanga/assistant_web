8a6c09d6d4aed14d12e84d593d3bad3340fe6e79
gefeiyanga <variousdid@gmail.com>
2023-07-08 22:09:21 +0800

    fix: 文件路径

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 
-import theme from "./theme";
+import theme from "@/styles/theme";
 
 export default function Document() {
   return (
