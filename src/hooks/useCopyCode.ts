d0426dbc36afcbf0bd86e8f4e9a3f0289c846d6b
gefeiyanga <variousdid@gmail.com>
2023-10-08 16:34:09 +0800

    fix: 修复复制代码时弹出多次提醒

--- src/hooks/useCopyCode.ts
+++ src/hooks/useCopyCode.ts
@@ -3,39 +3,42 @@ import { useToast } from "@chakra-ui/react";
 
 import { copyText } from "@/utils/util";
 
-const useCopyCode = (conversationList: any[]) => {
-  const toast = useToast({
-    position: "top",
-    duration: 3000,
-    variant: "subtle",
-    containerStyle: {
-      color: "#333333",
-      fontWeight: 700,
-    },
-  });
+const useCopyCode = (conversationList: any[], toast: any) => {
   useEffect(() => {
+    const cppyCallback = (codeBlock: any) => {
+      if (navigator.clipboard?.writeText) {
+        navigator.clipboard.writeText(codeBlock.textContent ?? "");
+        toast({
+          description: "复制成功",
+        });
+      } else {
+        copyText({ text: codeBlock.textContent ?? "", origin: true });
+        toast({
+          description: "复制成功",
+        });
+      }
+    };
     if (conversationList[conversationList?.length - 1]?.done) {
+      const codeBlockWrapper = document.querySelectorAll(".code-block-wrapper");
+      codeBlockWrapper.forEach((wrapper) => {
+        const copyBtn: any = wrapper.querySelector(".code-block-header__copy");
+        const codeBlock = wrapper.querySelector(".code-block-body");
+        if (copyBtn && codeBlock && !copyBtn["listenClick"]) {
+          copyBtn.addEventListener("click", () => cppyCallback(codeBlock));
+          copyBtn["listenClick"] = true;
+        }
+      });
+    }
+    return () => {
       const codeBlockWrapper = document.querySelectorAll(".code-block-wrapper");
       codeBlockWrapper.forEach((wrapper) => {
         const copyBtn = wrapper.querySelector(".code-block-header__copy");
         const codeBlock = wrapper.querySelector(".code-block-body");
         if (copyBtn && codeBlock) {
-          copyBtn.addEventListener("click", () => {
-            if (navigator.clipboard?.writeText) {
-              navigator.clipboard.writeText(codeBlock.textContent ?? "");
-              toast({
-                description: "复制成功",
-              });
-            } else {
-              copyText({ text: codeBlock.textContent ?? "", origin: true });
-              toast({
-                description: "复制成功",
-              });
-            }
-          });
+          copyBtn.removeEventListener("click", () => cppyCallback(codeBlock));
         }
       });
-    }
+    };
   }, [conversationList[conversationList?.length - 1]?.done]);
 };
 
